a103cfdb5640348aa5ae866de9dfed54303ad29b
Lahiru Himesh Madusanka <lahiru.lahirumadusanka@gmail.com>
2022-09-06 01:56:02 +0530

    Initial commit

---
+++ api/index.js
@@ -0,0 +1,18 @@
+export default function handler(request, response) {
+  const fs = require("fs");
+  const path = require("path");
+
+  fs.readdir(path.join(process.cwd(), "wallpapers"), (err, files) => {
+    let max = files.length - 1;
+    let min = 0;
+
+    let index = Math.round(Math.random() * (max - min) + min);
+    let file = files[index];
+
+    response.writeHead(302, {
+      Location: `wallpapers/${file}`,
+      //add other headers here...
+    });
+    response.end();
+  });
+}
